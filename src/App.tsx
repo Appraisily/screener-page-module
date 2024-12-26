7354edc9b179c38118a38c3fb9d66fcaf30fab55
Appraisily <134934621+Appraisily@users.noreply.github.com>
2024-12-26 12:25:13 +0100

    ssa

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AnalyzePage from './pages/AnalyzePage';
-import { useTawkTo } from './hooks/useTawkTo';
 
 function App() {
-  useTawkTo();
-
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
